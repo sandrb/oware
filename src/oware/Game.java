0b8f2724ae941cfa1f98eb2d646c62e6f0795574
sander <sander@groowup.nl>
2015-11-16 17:38:02 +0100

    First class

---
+++ src/oware/Game.java
@@ -0,0 +1,20 @@
+package oware;
+
+public class Game {
+	int[] piles = new int[12]; //values represent the piles, piles 0 - 5 are ours and piles 6 - 11 are the opponents
+	
+	/**
+	 * Sets the values for the current piles
+	 * @param input: the values for the current piles, 12 non-negative integers differentiated by spaces representing the 12 piles
+	 */
+	public void setPiles(String input){
+		String[] inputArray = input.split(" ");
+		
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		
+	}
+
+}
