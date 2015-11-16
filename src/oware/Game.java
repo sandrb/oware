686e5ad030cf22850e13b6d3f6e5cd981fd5b035
sander <sander@groowup.nl>
2015-11-16 17:49:13 +0100

    12 --> 24 Created newGame method

--- src/oware/Game.java
+++ src/oware/Game.java
@@ -1,15 +1,30 @@
 package oware;
 
+import java.util.Arrays;
+
 public class Game {
-	int[] piles = new int[12]; //values represent the piles, piles 0 - 5 are ours and piles 6 - 11 are the opponents
+	int maxSeeds = 96; //initial and maximum number of seeds
+	int[] piles = new int[24]; //values represent the piles, piles 0 - 11 are ours and piles 12 - 23 are the opponents
 	
 	/**
 	 * Sets the values for the current piles
-	 * @param input: the values for the current piles, 12 non-negative integers differentiated by spaces representing the 12 piles
+	 * @param input: the values for the current piles, 24 non-negative integers differentiated by spaces representing the 12 piles
 	 */
 	public void setPiles(String input){
 		String[] inputArray = input.split(" ");
 		//todo: string --> integer array
+	}
+	
+	/**
+	 * Initializes a new game with an equal number of seeds per pile.
+	 */
+	public void newGame(){
+		int seedsPerPile = maxSeeds / piles.length;//calculate max number of seeds per pile
+		for(int i =0; i < piles.length; i++){
+			piles[i] = seedsPerPile;//assign number to each pile
+		}
+		System.out.println("game created");
+		System.out.print(Arrays.toString(piles));
 	}
 	
 	/**
@@ -19,12 +34,14 @@ public class Game {
 		//todo println string		
 	}
 	
+	
 	private void computeNextMove(){
 		//place where all the work gets done.
 	}
 
 	public static void main(String[] args) {
 		Game game = new Game();
+		game.newGame();
 		//todo: initialize game.
 	}
 
