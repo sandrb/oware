26506095bbc7af3bcdffb6bcd56f863745cb23cd
sander <sander@groowup.nl>
2015-11-26 03:30:57 +0100

    Different approach, does not only take scores into account but also positions of our seeds and the opponents seeds, propably not optimal, but a lot better than what we had so far.

---
+++ src/oware2/Algorithm.java
@@ -0,0 +1,150 @@
+package oware2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Algorithm{
+	
+	Position initialPosition;//the input values computed to Position
+	
+	/**
+	 * Computes which pile to seed next
+	 * @param currentPosition: array of the current position
+	 * @param isProgramTurn: boolean, true if it is the programs turn
+	 * @param programScore: integer, current program score
+	 * @param inputScore: integer, current input player score
+	 * @return the position to seed
+	 * @pre first half of currentPosition belongs to input player, other half to computer player
+	 */
+	public int computeNextMove(int[] currentPosition, boolean isProgramTurn, int programScore, int inputScore){
+		initialPosition = new Position(currentPosition, isProgramTurn, programScore, inputScore);
+		int maxDepth = 5;
+		Position result = decisionTree(maxDepth,initialPosition);		
+		return result.movesDone.get(0);
+	}
+
+
+	/**
+	 * Computes which pile to seed next
+	 * @param curDepth: if 0, this will not go any deeper
+	 * @param initialPosition: the position to use
+	 * @return the position to seed
+	 * @pre first half of currentPosition belongs to input player, other half to computer player
+	 */
+	private Position decisionTree(int curDepth, Position initialPosition) {
+		ArrayList<Position> positions = generateAllPossibillities(initialPosition);
+		Position returnPosition = new Position();
+		Position compareValue;
+		if(curDepth > 0){
+			
+			//go for next level
+			if(initialPosition.isProgramTurn){
+				//program turn, pick maximum value
+				returnPosition.eveluationValue = -10000;
+				for(Position position : positions){
+					compareValue = decisionTree(curDepth - 1, position);
+					if(compareValue.eveluationValue > returnPosition.eveluationValue){
+						returnPosition = compareValue;
+					}
+				}	
+			}else{
+				//not program turn, pick minimum value
+				returnPosition.eveluationValue = 10000;
+				for(Position position : positions){
+					compareValue = decisionTree(curDepth - 1, position);
+					if(compareValue.eveluationValue < returnPosition.eveluationValue){
+						returnPosition = compareValue;
+					}
+				}	
+			}		
+		}else{//do eveluation function
+			//run evaluation function			
+			returnPosition = evaluation(initialPosition);
+		}
+		//System.out.println("depth:" + curDepth);
+		//printMovesDone(returnPosition);
+		return returnPosition;
+	}
+	
+	/**
+	 * @param position: input position
+	 * @return an arraylist with all possible positions after doing 1 move from the given position
+	 */
+	private ArrayList<Position> generateAllPossibillities(Position position){
+		ArrayList<Integer> options = new ArrayList<Integer>();
+		
+		if(!position.isProgramTurn){
+			//seed first half
+			for(int i = 0; i < position.piles.length / 2; i++){//for each position
+				if(position.piles[i] > 0){//check if it is "movable"
+					options.add(i);
+				}
+			}			
+		}else{
+			//seed second half
+			for(int i = 0; i < position.piles.length / 2; i++){//for each position
+				if(position.piles[i + position.piles.length / 2] > 0){//check if it is "movable"
+					options.add(i + position.piles.length / 2);
+				}
+			}
+		}
+		
+		ArrayList<Position> returnValue = new ArrayList<Position>();
+		if(options.size() > 0){
+			for(int option : options){//for each option
+				Position newPos = new Position();//create a new position
+				newPos.copy(position);//copy current values
+				newPos.play(option);//move this option
+				returnValue.add(newPos);//add this option to the arrayList
+			}			
+		}else{//found a final position, just return the same input position
+			returnValue.add(position);
+		}
+		
+		return returnValue; //return the returnValue		
+	}
+	
+	/**
+	 * fills in the evaluation value of the position, higher is better for us.
+	 * @param input: a position 
+	 * @return the same position with an evaluationValue
+	 */
+	private Position evaluation(Position input){
+		double evaluationScore = 0.0;
+		//step 1: give points based on the current score
+		evaluationScore = (double) input.programScore - (double) input.inputScore;
+		//step 2: low values in the beginning of our side are bad as they can be easily captured, good on the opponents side
+		for(int i = 0; i < 5; i++){
+			if(input.piles[i + input.piles.length / 2] == 1){
+				//can be formed into 2, assumed 75% chance
+				evaluationScore -= 0.75 * 2;
+			}else if(input.piles[i + input.piles.length / 2] == 2){
+				//can be formed into 3, assumed 75% chance
+				evaluationScore -= 0.75 * 3;				
+			}
+			if(input.piles[i] == 1){
+				//can be formed into 2, assumed 75% chance
+				evaluationScore += 0.75 * 2;
+			}else if(input.piles[i] == 2){
+				//can be formed into 3, assumed 75% chance
+				evaluationScore += 0.75 * 3;				
+			}
+		}
+		//step 3: high values at the end are good, 0.5 points for each seed in the last 5 positions
+		for(int i = 0; i < 5; i++){
+			evaluationScore += input.piles[input.piles.length - 1 - i] * 0.5;
+			evaluationScore -= input.piles[input.piles.length/2 - 1 - i] * 0.5;
+		}
+		//input.programScore
+		input.eveluationValue = evaluationScore;
+		return input;
+	}
+	
+	private void printMovesDone(Position input){
+		for(int move : input.movesDone){
+			System.out.print(move + " ");
+		}
+		System.out.println();
+	}
+	
+}
---
+++ src/oware2/GameSander.java
@@ -0,0 +1,213 @@
+package oware2;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameSander {
+	int maxSeeds = 96; //initial and maximum number of seeds
+	int[] piles = new int[24]; //values represents the piles, piles 0 - 11 are the users and piles 12 - 23 are the programs
+	int programScore; // The score of the program
+	int inputScore; // The score of the input
+	boolean isProgramTurn; //Next move is my turn or not
+	int depthMax = 4; //the maximal depth
+	Random randomGenerator = new Random();//Random Generator, used for testing
+	Scanner user_input = new Scanner( System.in );
+	Algorithm algorithm = new Algorithm();
+	
+	/**
+	 * Initializes a new game with an equal number of seeds per pile.
+	 */
+	public void newGame(){
+		int seedsPerPile = maxSeeds / piles.length;//calculate max number of seeds per pile
+		for(int i =0; i < piles.length; i++){
+			piles[i] = seedsPerPile;//assign number to each pile
+		}
+		System.out.println("Who starts? 0: computer, 1: player.");
+		String input = user_input.nextLine();
+		int start = Integer.parseInt(input);
+		outputPiles();
+		
+		programScore = 0;
+		inputScore = 0;	
+		
+		if(start == 1){
+			inputMove();
+		} else {
+			computerMove();
+		}
+	}
+		
+	/**
+	 * Let's the player do a move
+	 */
+	public void inputMove(){
+		isProgramTurn = false;
+		String options = "";
+		for(int i = 0; i < piles.length / 2; i++){//for each position
+			if(piles[i] > 0){//check if it is "movable"
+				options += " " + i;//if yes, add it to the options				
+			}
+		}
+		System.out.println("Select position to sow, options are:" + options);
+		String input = user_input.nextLine();
+		int position = Integer.parseInt(input);
+		int lastChanged = sow(position);
+		capture(lastChanged);//capture seeds if needed
+		
+		System.out.println();
+		outputPiles();
+
+		if(!hasWonLost()){//no winner yet, continue
+			computerMove();			
+		}		
+	}
+	
+	/**
+	 * Does a computer move
+	 */
+	public void computerMove(){
+		isProgramTurn = true;
+		ArrayList<Integer> options = new ArrayList<Integer>();
+		for(int i = 0; i < piles.length / 2; i++){//for each position
+			if(piles[i + piles.length / 2] > 0){//check if it is "movable"
+				options.add(i + piles.length / 2);
+			}
+		}
+		//for testing: sow a random integer from the options		
+        //int position = options.get(randomGenerator.nextInt(options.size()));
+		int position = algorithm.computeNextMove(piles, isProgramTurn, programScore, inputScore);
+		int lastChanged = sow(position);
+		capture(lastChanged);//capture seeds if needed
+		System.out.println("Computer sowed position " + position);
+		outputPiles();
+		if(!hasWonLost()){//no winner yet, continue
+			inputMove();			
+		}		
+	}	
+	
+	/**
+	 * returns the current piles in the same format as they are inputed
+	 */
+	public void outputPiles(){
+		System.out.print("Player 1 (u input): ");
+		for(int i = 0; i < 12; i++){
+			System.out.print(piles[i] + " ");
+		}
+		System.out.println();
+		System.out.print("Player 2 (program): ");
+		for(int i = 23; i >= 12; i--){
+			System.out.print(piles[i] + " ");
+		}
+		System.out.println();
+		System.out.println("Program: " + programScore + ", input: " + inputScore);
+		System.out.println();
+	}
+	
+	
+	/**
+	 * Sows a certain position, spreading it's seeds over the neighbors
+	 * @param pos_choose: position that is sowed.
+	 * @return: returns the latest pile whose seed count was increased.
+	 */
+	private int sow(int pos_choose){
+		if(pos_choose >= piles.length || pos_choose < 0){
+		   throw new IllegalArgumentException("Invalid input for sow, was " + pos_choose + ", should be 0 <= input < " + (piles.length/2));			
+		}
+		int lastChanged = 0;
+		int i = 0;
+		while(piles[pos_choose] > 0){
+			if((pos_choose+i+1) % piles.length != pos_choose){//skip pos_choose
+				piles[(pos_choose+i+1) % piles.length]++;//place one seed
+				lastChanged = (pos_choose+i+1) % piles.length;
+				piles[pos_choose]--;//remove one seed				
+			}
+			i++;//go on to the next position
+		}
+		return lastChanged;
+	}
+	
+	/**
+	 * Captures the seeds after a turn
+	 * @param lastChanged: pile lastly increased
+	 */
+	private void capture(int lastChanged){
+		//no calculation, actually capturing seeds now.		
+		boolean doCapture = true;//set to false if it is not needed.
+		int sumRemaining = 0;//sum of all the seeds remaining on the opponents side
+		if(isProgramTurn){
+			//the turn of the code
+			for(int i = 0; i < piles.length/2 && sumRemaining == 0; i++){
+				//get sum of all positions NOT seeded (only need to know if it's more than 0)
+				if(i > lastChanged){
+					sumRemaining += piles[i];
+				}else if(piles[i] != 2 && piles[i] != 3){
+					sumRemaining += piles[i];
+				}
+			}
+			if(sumRemaining == 0){
+				// if a move would capture all of an opponent's seeds, the capture is forfeited since this would prevent the opponent from continuing the game, and the seeds are instead left on the board
+				doCapture = false;
+			}
+			
+			if(doCapture){
+				while(lastChanged >= 0 && lastChanged < piles.length/2 && piles[lastChanged] >= 2 && piles[lastChanged] <= 3){
+					//continue as long as we are on users side and the number of seeds is 2 or 3
+					programScore += piles[lastChanged];
+					piles[lastChanged] = 0;	
+					lastChanged--;
+				}				
+			}
+		}else{
+			//the turn of the user input
+			for(int i = piles.length/2; i < piles.length && sumRemaining == 0; i++){
+				//get sum of all positions NOT seeded (only need to know if it's more than 0)
+				if(i > lastChanged){
+					sumRemaining += piles[i];
+				}else if(piles[i] != 2 && piles[i] != 3){
+					sumRemaining += piles[i];
+				}
+			}
+			if(sumRemaining == 0){
+				// if a move would capture all of an opponent's seeds, the capture is forfeited since this would prevent the opponent from continuing the game, and the seeds are instead left on the board
+				doCapture = false;
+			}
+			if(doCapture){
+				while(lastChanged >= piles.length/2 && piles[lastChanged] >= 2 && piles[lastChanged] <= 3){
+					//continue as long as we are on the programs side and the number of seeds is 2 or 3
+					inputScore += piles[lastChanged];
+					piles[lastChanged] = 0;
+					lastChanged--;				
+				}					
+			}		
+		}
+	}
+	
+	/**
+	 * Checks if the game has been won or lost
+	 * @return true if the game is finished, false otherwise
+	 */
+	private boolean hasWonLost(){
+		if(programScore > maxSeeds / 2){
+			//program has more than half the points
+			System.out.println("The program has won, user input has lost.");
+			return true;
+		}else if(inputScore > maxSeeds / 2){
+			//user has more than half the points
+			System.out.println("The user input has won, program has lost.");	
+			return true;			
+		}else if(inputScore == maxSeeds / 2 && programScore == maxSeeds / 2){
+			//points equally divided
+			System.out.println("The game resulted in a draw.");
+			return true;						
+		}else{
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		GameSander game = new GameSander();
+		game.newGame();
+	}
+
+}
