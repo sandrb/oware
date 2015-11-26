9e1bc77b902acbaa9dc76d8aee9c9905ed47117f
sander <sander@groowup.nl>
2015-11-26 04:44:04 +0100

    Implemented a test class which does random options instead of user input.

--- src/oware2/testGame.java
+++ src/oware2/testGame.java
@@ -1,15 +1,42 @@
 package oware2;
 
 public class testGame {
+	int result;
+	int won = 0;
+	int lost = 0;
+	int draw = 0;
 	
-	public void runTest(){
+	public void runTest(int amount){
+		for(int i = 0; i < amount; i++){
+			runSingleTest();
+		}
+		int total = won + lost + draw;
+		double percentage = won / total * 100;
+		System.out.println();
+		System.out.println("Total amount of games:" + total);
+		System.out.println("Amount won: " + won);
+		System.out.println("Amount lost: " + lost);
+		System.out.println("Amount draw: " + draw);
+		System.out.println("Percentage won: " + percentage + "%");
+		
+	}
+	
+	public void runSingleTest(){
 		GameSander game = new GameSander();
 		game.testModeOn();
 		game.newGame();
+		result = game.report();
+		if(result == 1){
+			won++;
+		}else if(result == 0){
+			draw++;
+		}else{
+			lost++;
+		}
 	}
 	
 	public static void main(String[] args) {
 		testGame test = new testGame();
-		test.runTest();
+		test.runTest(10);
 	}
 }
